c605af162eb1ce2815b0190ca24fb582bb91bbdb
Lucas-PC <lgbs@cesar.school>
2024-10-12 00:09:52 -0300

    Limpando o nome das colunas

--- jobs/download_dataset.py
+++ jobs/download_dataset.py
@@ -1,12 +1,29 @@
+import os
 from kaggle.api.kaggle_api_extended import KaggleApi
+import pandas as pd
 
 api = KaggleApi()
 api.authenticate()
 
 # Define o caminho desejado para salvar o dataset
 desired_path = "data"
+url_data_set = 'fatemehmehrparvar/obesity-levels'
 
 # Baixa o dataset no caminho especificado
-api.dataset_download_files("mattop/nutrition-physical-activity-and-obesity", path=desired_path, unzip=True)
+api.dataset_download_files(url_data_set, path=desired_path, unzip=True) # Baixa o dataset
 
-print("Dataset baixado em:", desired_path)
+downloaded_files = os.listdir(desired_path) # Lista os arquivos baixados
+
+for file in downloaded_files:
+    print("Arquivo baixado:", file)
+
+# Renomeia as colunas do dataset
+try:
+    df = pd.read_csv(f'{desired_path}/{file}') # Faz a leitura do dataset
+    df.columns = [col.lower().replace(" ", "").replace("(", "_").replace(")", "").replace("/", "_") for col in df.columns] # Renomeia as colunas tirando caracteres especiais e espaços
+    # TODO: Limpar os dados antes de fazer a inserção no banco de dados
+    
+    df.to_csv(f'{desired_path}/{file}', index=False)
+    print("Colunas renomeadas com sucesso!")
+except Exception as e:
+    print(f"Erro ao renomear as colunas: {e}")
